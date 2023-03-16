58864a627697881a36343dcdda6ac907dcee77b9
Jakub.Poznanski <jqbpoz02@gmail.com>
2023-03-16 16:15:24 +0100

    replace couts to display funtion

--- zad1/zad1_a.cpp
+++ zad1/zad1_a.cpp
@@ -1,14 +1,7 @@
 // Jakub Poznanski
-#include <iostream>
-#include <unistd.h>
-using namespace std;
+#include "display.h"
 int main()
 {
-    cout << "UID: " << getuid() << endl;
-    cout << "GID: " << getgid() << endl;
-    cout << "PID: " << getpid() << endl;
-    cout << "PPID: " << getppid() << endl;
-    // getpgid(0) zwraca PGID procesu o podanym PID (w tym przypadku bieżącego procesu)
-    cout << "PGID: " << getpgid(0) << endl;
+    display();
     return 0;
 }
