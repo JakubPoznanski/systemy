43eb2a80e78ed17952af72032b0b577c294535ae
Jakub.Poznanski <jqbpoz02@gmail.com>
2023-03-16 11:49:19 +0100

    zad1_b finished

--- zad1/zad1_b.cpp
+++ zad1/zad1_b.cpp
@@ -1,36 +1,31 @@
-#include <stdio.h>
+//Jakub Poznanski 
+#include <iostream>
 #include <unistd.h>
 #include <sys/wait.h>
-
-int main() {
-    int i;
-    pid_t pid;
-
-    for (i = 0; i < 3; i++) {
-        pid = fork();
-
-        if (pid < 0) {
-            // błąd podczas tworzenia procesu potomnego
-            printf("Błąd podczas tworzenia procesu potomnego!\n");
-            return 1;
-        } else if (pid == 0) {
-            // kod procesu potomnego
-            printf("UID: %d\n", getuid());
-            printf("GID: %d\n", getgid());
-            printf("PID: %d\n", getpid());
-            printf("PPID: %d\n", getppid());
-            printf("PGID: %d\n", getpgid(getpid()));
-            return 0;
-        } else {
-            // kod procesu macierzystego
-            printf("Proces macierzysty - PID: %d, utworzył proces potomny - PID: %d\n", getpid(), pid);
-        }
-    }
-    printf("Początek pętli z waitem");
-    // czekanie na zakończenie wszystkich procesów potomnych
-    for (i = 0; i < 3; i++) {
-        wait(NULL);
-    }
-
-    return 0;
+using namespace std;
+int main () {
+	
+	cout << "\nProces macierzysty" <<endl;
+	cout << "UID:  " << getuid() << endl;
+	cout << "GID:  " << getgid() << endl;
+	cout << "PID:  " << getpid() << endl;
+    cout << "PPID: " << getppid() << endl;
+	for (int i = 0; i < 3; i++) {
+		
+		switch (fork ()) {
+			case -1:
+				perror("fork error");
+				exit(1);
+				
+			case 0:
+	        cout << "\nUID:  " << getuid() << endl;
+            cout << "GID:  " << getgid() << endl;
+            cout << "PID:  " << getpid() << endl;
+            cout << "PPID: " << getppid() << endl;
+				break;
+				
+			default:
+			wait(NULL);
+		}
+	}
 }
