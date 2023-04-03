b315829adfc2f6cb647baa04a0711b7b4c1120fa
Jakub.Poznanski <jqbpoz02@gmail.com>
2023-04-03 11:38:19 +0200

    edit zad3_a

--- zad3/zad3_a.cpp
+++ zad3/zad3_a.cpp
@@ -4,26 +4,57 @@
 #include <stdio.h>
 #include <signal.h>
 #include <unistd.h>
-// SIGQUIT - Sekwencja wyjścia z klawiatury
-// SIGINT - Przerwanie z klawiatury
-// SIGTSTP - Sekwencja zatrzymania z klawiatury
-// kill -9 <PID> można użyć z innego terminalu
+#include <string.h>
+// Przykładowe sygnały i ich numery
+//  2 - SIGINT - Przerwanie z klawiatury [ctrl+c]
+//  3 - SIGQUIT - Sekwencja wyjścia z klawiatury [ctrl+\]
+//  20 - SIGTSTP - Sekwencja zatrzymania z klawiatury [ctrl+z]
+//  kill -9 <PID> można użyć z innego terminalu
 void custom(int sig)
 {
-    std::cout << "\nPrzechwycono sygnał SIGTSTP" << std::endl;
+    std::cout << "Przechwycenie sygnału:\nNumer: " << sig << "\nRodzaj: " << strsignal(sig) << std::endl;
 };
-
-int main()
+int main(int argc, char *argv[])
 {
+    if (argc != 3)
+    {
+        std::cout << "Brak argumentów wywołania\nPodaj numer sygnału , sposób obsługi" << std::endl;
+        exit(0);
+    }
+    int action = atoi(argv[2]);
+    int number = atoi(argv[1]);
     std::cout << " PID:" << getpid() << std::endl;
-    // Dla sygnału SIGQUIT wykonaj operację domyślą [ctrl+\]
-    signal(SIGQUIT, SIG_DFL);
-    // Zignoruj sygnał SIG_IGN [ctrl+c]
-    signal(SIGINT, SIG_IGN);
-    // Dla sygnału SIGTSTP wykonaj funkcję custom [ctrl+z]
-    signal(SIGTSTP, custom);
 
-    // Czekaj na sygnały
+    switch (action)
+    {
+    case 1:
+        if (signal(number, SIG_DFL) == SIG_ERR)
+        {
+            perror("signal err");
+            exit(0);
+        }
+        std::cout << "Operacja domyślna dla sygnału:\nNumer: " << number << "\nRodzaj: " << strsignal(number) << std::endl;
+        break;
+    case 2:
+        if (signal(number, SIG_IGN) == SIG_ERR)
+        {
+            perror("signal err");
+            exit(0);
+        }
+        std::cout << "Ignorowanie sygnału:\nNumer: " << number << "\nRodzaj: " << strsignal(number) << std::endl;
+        break;
+    case 3:
+        if (signal(number, custom) == SIG_ERR)
+        {
+            perror("signal err");
+            exit(0);
+        }
+        break;
+    default:
+        std::cout << "błędne podanie rodzaju sygnału\n1-operacja domyślna\n2-ignorowanie\n3-operacja przechwycenia" << std::endl;
+        exit(0);
+        break;
+    }
     while (1)
     {
         pause();
