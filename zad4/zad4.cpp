4cb83ebba15c437f0598c1acc1b22d06c3526ae3
Jakub.Poznanski <jqbpoz02@gmail.com>
2023-04-16 16:37:09 +0200

    final version

--- zad4/zad4.cpp
+++ zad4/zad4.cpp
@@ -1,4 +1,3 @@
-
 // Jakub Poznanski
 #include <iostream>
 #include <stdio.h>
@@ -14,7 +13,7 @@ int main(int argc, char *argv[])
 {
     int bytesForBufP = 3;
     int bytesForBufC = 3;
-    int id, filedes[2], nobytes, inFileDescriptor, outFileDescriptor, random_value;
+    int id, filedes[2], bytesread, inFileDescriptor, outFileDescriptor, random_value;
     char bufferP[bytesForBufP], bufferC[bytesForBufC];
     char bytesNumber[2];
 
@@ -40,26 +39,26 @@ int main(int argc, char *argv[])
             std::cout << "\n Oczekiwanie przez: " << random_value + 1 << " sekundy \n " << std::endl;
             sleep(random_value + 1);
             // Odczyt bitów z wyjścia potoku i zapis w buforze
-            nobytes = read(filedes[0], &bufferC, bytesForBufC);
+            bytesread = read(filedes[0], &bufferC, bytesForBufC);
             // Wpis do pliku wyjściowego
-            if (write(outFileDescriptor, &bufferC, nobytes) == -1)
+            if (write(outFileDescriptor, &bufferC, bytesread) == -1)
             {
                 perror("write error\n");
                 exit(0);
             }
             // wpis bufora na standardowe wyjście
-            if (write(STDOUT_FILENO, &bufferC, nobytes) == -1)
+            if (write(STDOUT_FILENO, &bufferC, bytesread) == -1)
             {
                 perror("write error\n");
                 exit(0);
             }
-            sprintf(bytesNumber, "%d", nobytes);
-            if (write(STDOUT_FILENO, &bytesNumber, nobytes) == -1)
+            sprintf(bytesNumber, "%d", bytesread);
+            if (write(STDOUT_FILENO, &bytesNumber, bytesread) == -1)
             {
                 perror("write error\n");
                 exit(EXIT_FAILURE);
             }
-        } while (nobytes == bytesForBufC);
+        } while (bytesread == bytesForBufC);
         close(outFileDescriptor);
         exit(0);
 
@@ -72,21 +71,21 @@ int main(int argc, char *argv[])
             random_value = rand() % 3;
             sleep(random_value + 1);
             // Odczyt bitów z pliku wejściowego i zapis w buforze
-            nobytes = read(inFileDescriptor, &bufferP, bytesForBufP);
+            bytesread = read(inFileDescriptor, &bufferP, bytesForBufP);
             // wpis z bufora do wejscia potoku
-            if (write(filedes[1], &bufferP, nobytes) == -1)
+            if (write(filedes[1], &bufferP, bytesread) == -1)
             {
                 perror("write error\n");
                 exit(0);
             }
             // wpis bufora na standardowe wyjście
-            if (write(STDOUT_FILENO, &bufferP, nobytes) == -1)
+            if (write(STDOUT_FILENO, &bufferP, bytesread) == -1)
             {
                 perror("write error\n");
                 exit(0);
             }
 
-        } while (nobytes == bytesForBufP);
+        } while (bytesread == bytesForBufP);
         close(inFileDescriptor);
         exit(0);
     }
